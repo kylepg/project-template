d62fc0798598a345bc122f089cadba15d49c6940
kylepg <kylepgeorge@gmail.com>
2020-01-14 13:59:22 -0500

    multifetch update

--- src/js/_helpers.js
+++ src/js/_helpers.js
@@ -5,7 +5,13 @@ export async function multiFetch(endpoints) {
     if (window.location.href.includes(`localhost`) || url.includes(`.loc`) || url.includes(`10.10.226`)) {
       url = endpoint.local;
     }
-    promises.push(fetch(url).then(res => res.json()));
+    promises.push(fetch(url).then(res => res.json()).catch(() => {
+      if (endpoint.fallback !== undefined) {
+        return fetch(endpoint.fallback).then(res => res.json());
+      }
+      console.error(`No fallback defined for ${url}. Returning blank JSON.`);
+      return {};
+    }));
   });
   const returnObj = {};
   await Promise.all(promises).then((data) => {
@@ -15,6 +21,7 @@ export async function multiFetch(endpoints) {
   });
   return returnObj;
 }
+
 
 export default () => {};
 
