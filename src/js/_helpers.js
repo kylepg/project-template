983695b695598c9a828f2924c54c4c0f867dcaff
Kyle George <kgeorge@Kyles-MacBook-Air.local>
2020-08-26 09:35:21 -0400

    Removed tailwind and vue

--- src/js/_helpers.js
+++ src/js/_helpers.js
@@ -22,6 +22,34 @@ export async function multiFetch(endpoints) {
   return returnObj;
 }
 
+export function updateQueryString(key, value, url) {
+  if (!url) url = window.location.href;
+  const re = new RegExp(`([?&])${key}=.*?(&|#|$)(.*)`, `gi`);
+  let hash;
+
+  if (re.test(url)) {
+    if (typeof value !== `undefined` && value !== null) {
+      return url.replace(re, `$1${key}=${value}$2$3`);
+    }
+    hash = url.split(`#`);
+    url = hash[0].replace(re, `$1$3`).replace(/(&|\?)$/, ``);
+    if (typeof hash[1] !== `undefined` && hash[1] !== null) {
+      url += `#${hash[1]}`;
+    }
+    return url;
+  }
+  if (typeof value !== `undefined` && value !== null) {
+    const separator = url.indexOf(`?`) !== -1 ? `&` : `?`;
+    hash = url.split(`#`);
+    url = `${hash[0] + separator + key}=${value}`;
+    if (typeof hash[1] !== `undefined` && hash[1] !== null) {
+      url += `#${hash[1]}`;
+    }
+    return url;
+  }
+  return url;
+}
+
 
 export default () => {};
 
